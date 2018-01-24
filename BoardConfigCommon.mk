592c19b4f79ca24d15fae50918f0f6f4bbae08b3
Naina Nalluri <nainanalluri@google.com>
2018-01-24 13:11:47 -0800

    sm6225-common: Set the flag to disable rild Set the flag to enable vendor equivalent ril service - i.e to disable rild
    Bug: 73299369
    Test: Compile and check the out folder
    Change-Id: I43d38c2d334b5988800da9de49f467cf3beadf24

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,6 +117,9 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
