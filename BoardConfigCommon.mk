bd08f0e4449cadffcb44205adf69702ae2cfe52b
Nahhk <nahhk@proton.me>
2024-07-06 11:59:35 -0500

    sm6225-common: Inherit `sepolicy_vndr` for qcom devices
    Change-Id: I753b05166f7ba63d87d9a6f83f44e5145491fea5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,9 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
