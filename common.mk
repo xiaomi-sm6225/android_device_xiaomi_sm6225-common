f667664e2cd4d574fbd557bceab1edb1da89760f
Nahhk <nahhk@proton.me>
2024-08-25 21:45:04 -0500

    sm6225-common: Import `hardware/xiaomi` namespace
    Change-Id: I9aca52286bcf8dd2efad42245dfff36cda02c24e

--- common.mk
+++ common.mk
@@ -65,7 +65,8 @@ PRODUCT_SHIPPING_API_LEVEL := 33
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # Update engine
 PRODUCT_PACKAGES += \
