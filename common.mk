15b8b5d8048fdecb98ea971b5d4a93c30a3f4251
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    sm6225-common: Create mount points out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- common.mk
+++ common.mk
@@ -150,6 +150,12 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     WifiResTarget
 
+# Mountpoints
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
