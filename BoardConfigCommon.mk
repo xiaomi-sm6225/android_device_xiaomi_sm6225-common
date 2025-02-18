b762b0324321c6d864c9f3a111e486ec2f52ca79
Nahhk <nahhk@proton.me>
2025-02-18 12:46:52 -0500

    sm6225-common: Setup vintf manifest and common fcm configs
    Change-Id: Ic3308e6a89645464bd10a6915117cfa7a58d2e71

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,6 +104,19 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
+DEVICE_MANIFEST_FILE := \
+    $(COMMON_PATH)/vintf/manifest.xml \
+    $(COMMON_PATH)/vintf/manifest_xiaomi.xml
+
+DEVICE_MATRIX_FILE := \
+    hardware/qcom-caf/common/compatibility_matrix.xml
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
