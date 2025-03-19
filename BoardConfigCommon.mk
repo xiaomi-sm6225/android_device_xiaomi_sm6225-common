7117b475a2ed14834c1e5945d7ad0db8ff12d19c
Nahhk <nahhk@proton.me>
2025-03-19 06:23:56 -0500

    sm6225-common: Initial common configuration
    Change-Id: I0adf216d82eca438a4d2c76349439c46830c08e9

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm6225-common/BoardConfigVendor.mk
+
+COMMON_PATH := device/xiaomi/sm6225-common
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := bengal
+TARGET_NO_BOOTLOADER := true
+
+# Hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := bengal
+TARGET_BOARD_SUFFIX := _515
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm6225-common/BoardConfigVendor.mk
---
+++ common.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
+# Enforce generic ramdisk allow list
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/sm6225-common/sm6225-common-vendor.mk)
