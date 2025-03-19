cbf63c683bfe6fd46def6b77cb7a93ef3d92f8fb
Nahhk <nahhk@proton.me>
2025-03-19 06:23:56 -0500

    sm6225-common: Initial common configuration
    Change-Id: I0adf216d82eca438a4d2c76349439c46830c08e9

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm6225-common/BoardConfigVendor.mk
+
+COMMON_PATH := device/xiaomi/sm6225-common
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a9
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := bengal
+
+# Hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Platform
+TARGET_BOARD_PLATFORM := bengal
+TARGET_BOARD_SUFFIX := _515
