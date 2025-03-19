0b0b2e9afd166ae5c543d03616edddbb2c1f0b0f
Nahhk <nahhk@proton.me>
2025-03-19 06:23:56 -0500

    sm6225-common: Initial common configuration
    Change-Id: I0adf216d82eca438a4d2c76349439c46830c08e9

---
+++ common.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sm6225-common/sm6225-common-vendor.mk)
