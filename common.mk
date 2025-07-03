043ff21043fbed30318098f41600d58720109d40
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-03 15:55:22 +0000

    sm6225-common: Define soong namespaces
    Change-Id: I8ce9ccac3f99e172875ea644590f4b6a91bd5bc5

--- common.mk
+++ common.mk
@@ -150,6 +150,11 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     WifiResTarget
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH) \
+    hardware/xiaomi
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
