942096157bc6f56d17f8140964c659f54325da76
AMWolfstein <ahmedhero4040@gmail.com>
2025-07-12 00:41:57 +0300

    sm6225-common: Add public libraries from stock
    * From creek OS2.0.207.0.VBOMIXM
    
    Change-Id: I03815a25c0cc186c0fb3054c8795f775147641cd

--- common.mk
+++ common.mk
@@ -170,6 +170,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+    $(LOCAL_PATH)/configs/public.libraries-qti.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries-qti.txt
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
