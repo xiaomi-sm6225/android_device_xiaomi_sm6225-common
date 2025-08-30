339ffc3efa8c3ea40d55fd916b703cba15c7517b
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-08-30 21:43:39 +0000

    sm6225-common: Copy network permissions
    Change-Id: Ifa40ca174067574e9774819e1885892e59eea7d6

--- common.mk
+++ common.mk
@@ -158,6 +158,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
+# Network
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # Overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
