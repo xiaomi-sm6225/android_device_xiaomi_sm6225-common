01481d965c6266cff7c9fb366e486a2a3b713903
ReStranger <restranger@disroot.org>
2025-10-01 01:03:03 +0300

    sm6225-common: Include missing common makefiles
    Change-Id: I5f0af3652292e183d8aeae7a267b949ab3eeeb2d
    Signed-off-by: ReStranger <restranger@disroot.org>

--- common.mk
+++ common.mk
@@ -4,6 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
