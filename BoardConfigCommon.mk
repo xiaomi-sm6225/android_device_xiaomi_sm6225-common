67b1bbe62228c05974b3c0e6eb846500341e1d88
ReStranger <restranger@disroot.org>
2025-11-04 05:02:21 +0300

    sm6225-common: sepolicy: Initial import sepolicy
    Change-Id: Id0975fa225f837ae4ef4d6795e7a479caf09dc2b
    Signed-off-by: ReStranger <restranger@disroot.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -167,6 +167,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
